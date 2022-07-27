dc56bf6191e99142f9d1b8c2ded817ad596172f5
Josh Justice <me@codingitwrong.com>
2022-07-26 21:25:40 -0400

    Test drive loading

--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -1,33 +1,71 @@
-import {render, screen} from '@testing-library/react-native';
+import {
+  act,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react-native';
+import flushPromises from 'flush-promises';
 import RestaurantList from './RestaurantList';
 import api from './api';
 
 jest.mock('./api');
 
 describe('RestaurantList', () => {
-  describe('when loading succeeds', () => {
-    it('renders restaurants from the server', async () => {
-      const restaurants = [
-        {id: 1, name: 'Pizza Place'},
-        {id: 2, name: 'Salad Place'},
-      ];
+  const restaurants = [
+    {id: 1, name: 'Pizza Place'},
+    {id: 2, name: 'Salad Place'},
+  ];
 
+  describe('while loading', () => {
+    it('shows a loading indicator', async () => {
+      const neverSettles = new Promise(() => {});
+      api.get.mockReturnValue(neverSettles);
+
+      render(<RestaurantList />);
+
+      expect(screen.queryByText('Loading…')).not.toBeNull();
+    });
+  });
+
+  describe('when loading succeeds', () => {
+    async function renderRestaurants() {
       api.get.mockResolvedValue({data: restaurants});
 
       render(<RestaurantList />);
 
-      await screen.findByText(restaurants[0].name);
+      await act(flushPromises);
+    }
+
+    it('renders restaurants from the server', async () => {
+      await renderRestaurants();
+      expect(screen.queryByText(restaurants[0].name)).not.toBeNull();
       expect(screen.queryByText(restaurants[1].name)).not.toBeNull();
+    });
+
+    it('hides the loading indicator', async () => {
+      await renderRestaurants();
+
+      expect(screen.queryByText('Loading…')).toBeNull();
     });
   });
 
   describe('when loading fails', () => {
-    it('renders an error message', async () => {
+    async function renderRestaurants() {
       api.get.mockRejectedValue();
 
       render(<RestaurantList />);
+    }
+
+    it('renders an error message', async () => {
+      renderRestaurants();
 
       await screen.findByText('An error occurred loading restaurants');
+    });
+
+    it('hides the loading indicator', async () => {
+      renderRestaurants();
+
+      await waitForElementToBeRemoved(() => screen.getByText('Loading…'));
     });
   });
 });
