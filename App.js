973c8073487d3ad1bb3c802781fd9ec73d3a7c87
Josh Justice <me@codingitwrong.com>
2022-07-27 13:39:27 -0400

    Add RN Paper

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import RestaurantListContainer from './src/RestaurantListContainer';
 
 export default function App() {
   return (
-    <>
+    <PaperProvider>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.safeArea}>
         <RestaurantListContainer />
       </SafeAreaView>
-    </>
+    </PaperProvider>
   );
 }
 
