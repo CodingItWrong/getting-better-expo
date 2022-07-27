d9e54c35c355bec57d7878adf9a70aa3de899e12
Josh Justice <me@codingitwrong.com>
2022-07-26 21:25:40 -0400

    Test drive loading

--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -2,27 +2,55 @@ import {render, screen} from '@testing-library/react-native';
 import RestaurantList from './RestaurantList';
 
 describe('RestaurantList', () => {
-  describe('when loading succeeds', () => {
-    it('renders restaurants from the server', () => {
-      const restaurants = [
-        {id: 1, name: 'Pizza Place'},
-        {id: 2, name: 'Salad Place'},
-      ];
+  const restaurants = [
+    {id: 1, name: 'Pizza Place'},
+    {id: 2, name: 'Salad Place'},
+  ];
 
+  describe('while loading', () => {
+    it('shows a loading indicator', async () => {
+      render(<RestaurantList loading />);
+
+      expect(screen.queryByText('Loading…')).not.toBeNull();
+    });
+  });
+
+  describe('when loading succeeds', () => {
+    function renderRestaurants() {
       render(<RestaurantList restaurants={restaurants} />);
+    }
+
+    it('renders restaurants from the server', () => {
+      renderRestaurants();
 
       expect(screen.queryByText(restaurants[0].name)).not.toBeNull();
       expect(screen.queryByText(restaurants[1].name)).not.toBeNull();
     });
+
+    it('hides the loading indicator', () => {
+      renderRestaurants();
+
+      expect(screen.queryByText('Loading…')).toBeNull();
+    });
   });
 
   describe('when loading fails', () => {
-    it('renders an error message', () => {
+    function renderRestaurants() {
       render(<RestaurantList loadError />);
+    }
+
+    it('renders an error message', () => {
+      renderRestaurants();
 
       expect(
         screen.queryByText('An error occurred loading restaurants'),
       ).not.toBeNull();
+    });
+
+    it('hides the loading indicator', () => {
+      renderRestaurants();
+
+      expect(screen.queryByText('Loading…')).toBeNull();
     });
   });
 });
