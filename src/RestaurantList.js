49579ead75c9e959ef37cf99fc16e55c5c4bc137
Josh Justice <me@codingitwrong.com>
2022-07-27 12:23:43 -0400

    BAD - Add basic styling

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -1,5 +1,12 @@
 import {useEffect, useState} from 'react';
-import {FlatList, Pressable, Text, TextInput, View} from 'react-native';
+import {
+  FlatList,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import api from './api';
 
 export default function RestaurantList() {
@@ -31,36 +38,43 @@ export default function RestaurantList() {
   }
 
   return (
-    <>
-      <TextInput
-        placeholder="New restaurant name"
-        value={name}
-        onChangeText={setName}
-      />
-      <Pressable
-        onPress={() =>
-          api
-            .post('/restaurants', {name})
-            .then(() => api.get('/restaurants'))
-            .then(response => {
-              setData(response.data);
-              setName('');
-            })
-            .catch(() =>
-              setUpdateErrorMessage('An error occurred adding the restaurant'),
-            )
-        }
-      >
-        <Text>Add</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={styles.addRow}>
+        <TextInput
+          placeholder="New restaurant name"
+          value={name}
+          onChangeText={setName}
+          style={styles.newRestaurantNameField}
+        />
+        <Pressable
+          style={[styles.button, styles.addButton]}
+          onPress={() =>
+            api
+              .post('/restaurants', {name})
+              .then(() => api.get('/restaurants'))
+              .then(response => {
+                setData(response.data);
+                setName('');
+              })
+              .catch(() =>
+                setUpdateErrorMessage(
+                  'An error occurred adding the restaurant',
+                ),
+              )
+          }
+        >
+          <Text>Add</Text>
+        </Pressable>
+      </View>
       {updateErrorMessage && <Text>{updateErrorMessage}</Text>}
       <FlatList
         data={data}
         keyExtractor={item => item.id}
         renderItem={({item}) => (
-          <View>
-            <Text>{item.name}</Text>
+          <View style={styles.restaurantRow}>
+            <Text style={styles.restaurantName}>{item.name}</Text>
             <Pressable
+              style={styles.button}
               onPress={() =>
                 api
                   .delete(`/restaurants/${item.id}`)
@@ -78,6 +92,47 @@ export default function RestaurantList() {
           </View>
         )}
       />
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  addRow: {
+    flexDirection: 'row',
+    alignContent: 'end',
+    padding: 8,
+  },
+  newRestaurantNameField: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 4,
+  },
+  restaurantRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+  },
+  restaurantName: {
+    flex: 1,
+    fontSize: 18,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    backgroundColor: '#eee',
+  },
+  addButton: {
+    marginLeft: 8,
+  },
+});
