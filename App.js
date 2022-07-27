2e576b7bf7db51a4836bde85622c38870f2e0f3d
Josh Justice <me@codingitwrong.com>
2022-07-27 12:23:43 -0400

    Add basic styling

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import RestaurantList from './src/RestaurantList';
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
         <RestaurantList />
       </SafeAreaView>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
