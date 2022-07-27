684b7a637c18d96ad37de75b0e5548db3b756d7b
Josh Justice <me@codingitwrong.com>
2022-07-26 21:25:40 -0400

    Test drive loading

--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -1,23 +1,40 @@
-import {render, screen} from '@testing-library/react-native';
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react-native';
 import RestaurantList from './RestaurantList';
 import api from './api';
 
 jest.mock('./api');
 
 describe('RestaurantList', () => {
+  const restaurants = [
+    {id: 1, name: 'Pizza Place'},
+    {id: 2, name: 'Salad Place'},
+  ];
+
+  describe('while loading', () => {
+    it('shows a loading indicator', async () => {
+      const neverSettles = new Promise(() => {});
+      api.get.mockReturnValue(neverSettles);
+
+      render(<RestaurantList />);
+
+      expect(screen.queryByText('Loading…')).not.toBeNull();
+    });
+  });
+
   describe('when loading succeeds', () => {
     it('renders restaurants from the server', async () => {
-      const restaurants = [
-        {id: 1, name: 'Pizza Place'},
-        {id: 2, name: 'Salad Place'},
-      ];
-
       api.get.mockResolvedValue({data: restaurants});
 
       render(<RestaurantList />);
 
       await screen.findByText(restaurants[0].name);
       expect(screen.queryByText(restaurants[1].name)).not.toBeNull();
+
+      expect(screen.queryByText('Loading…')).toBeNull();
     });
   });
 
@@ -28,6 +45,8 @@ describe('RestaurantList', () => {
       render(<RestaurantList />);
 
       await screen.findByText('An error occurred loading restaurants');
+
+      expect(screen.queryByText('Loading…')).toBeNull();
     });
   });
 });
