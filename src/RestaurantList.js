45d8e503ebf338a291b2883a3178c659fa35e149
Josh Justice <me@codingitwrong.com>
2022-07-30 10:30:27 -0400

    Test-drive initial data load

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -1,3 +1,11 @@
-export default function RestaurantList() {
-  return null;
+import {FlatList, Text} from 'react-native';
+
+export default function RestaurantList({restaurants}) {
+  return (
+    <FlatList
+      data={restaurants}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => <Text>{item.name}</Text>}
+    />
+  );
 }
--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -1,4 +1,18 @@
 import {render, screen} from '@testing-library/react-native';
 import RestaurantList from './RestaurantList';
 
-describe('RestaurantList', () => {});
+describe('RestaurantList', () => {
+  const restaurants = [
+    {id: 1, name: 'Pizza Place'},
+    {id: 2, name: 'Salad Place'},
+  ];
+
+  describe('when loading succeeds', () => {
+    it('renders restaurants from the server', () => {
+      render(<RestaurantList restaurants={restaurants} />);
+
+      expect(screen.queryByText(restaurants[0].name)).not.toBeNull();
+      expect(screen.queryByText(restaurants[1].name)).not.toBeNull();
+    });
+  });
+});
