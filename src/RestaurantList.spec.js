e7d46a292c095cb59e9bcb846242f0ba1645d924
Josh Justice <me@codingitwrong.com>
2022-07-30 10:30:27 -0400

    Test-drive initial data load

--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -1,4 +1,23 @@
 import {render, screen} from '@testing-library/react-native';
 import RestaurantList from './RestaurantList';
+import api from './api';
 
-describe('RestaurantList', () => {});
+jest.mock('./api');
+
+describe('RestaurantList', () => {
+  describe('when loading succeeds', () => {
+    it('renders restaurants from the server', async () => {
+      const restaurants = [
+        {id: 1, name: 'Pizza Place'},
+        {id: 2, name: 'Salad Place'},
+      ];
+
+      api.get.mockResolvedValue({data: restaurants});
+
+      render(<RestaurantList />);
+
+      await screen.findByText(restaurants[0].name);
+      expect(screen.queryByText(restaurants[1].name)).not.toBeNull();
+    });
+  });
+});
