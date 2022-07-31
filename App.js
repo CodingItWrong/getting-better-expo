961d59bb0e3d8286843157a6f603397518a003a6
Josh Justice <me@codingitwrong.com>
2022-07-31 07:15:02 -0400

    Add React Query

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import RestaurantListContainer from './src/RestaurantListContainer';
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <PaperProvider>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.safeArea}>
-        <RestaurantListContainer />
-      </SafeAreaView>
-    </PaperProvider>
+    <QueryClientProvider client={queryClient}>
+      <PaperProvider>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView style={styles.safeArea}>
+          <RestaurantListContainer />
+        </SafeAreaView>
+      </PaperProvider>
+    </QueryClientProvider>
   );
 }
 
