81e6486a043f3bc17659c17d98fb12d10591486e
Josh Justice <me@codingitwrong.com>
2022-08-25 14:54:49 -0600

    BAD - Add styling

--- src/RestaurantList.js
+++ src/RestaurantList.js
@@ -32,32 +32,42 @@ export default function RestaurantList({
   }
 
   return (
-    <>
-      <TextInput
-        placeholder="New restaurant name"
-        value={name}
-        onChangeText={setName}
-      />
-      <Pressable
-        testID="add-button"
-        disabled={adding}
-        onPress={() => {
-          setAdding(true);
-          api
-            .post('/restaurants', {name})
-            .then(() => reloadRestaurants())
-            .then(() => {
-              setName('');
-              setAdding(false);
-            })
-            .catch(() =>
-              setUpdateErrorMessage('An error occurred adding the restaurant'),
-            );
-        }}
-      >
-        <Text>{adding ? 'Adding…' : 'Add'}</Text>
-      </Pressable>
-      {updateErrorMessage && <Text>{updateErrorMessage}</Text>}
+    <View style={styles.container}>
+      <View style={styles.addRow}>
+        <TextInput
+          placeholder="New restaurant name"
+          value={name}
+          onChangeText={setName}
+          style={styles.newRestaurantNameField}
+        />
+        <Pressable
+          testID="add-button"
+          disabled={adding}
+          style={[styles.button, styles.addButton]}
+          onPress={() => {
+            setAdding(true);
+            api
+              .post('/restaurants', {name})
+              .then(() => reloadRestaurants())
+              .then(() => {
+                setName('');
+                setAdding(false);
+              })
+              .catch(() =>
+                setUpdateErrorMessage(
+                  'An error occurred adding the restaurant',
+                ),
+              );
+          }}
+        >
+          <Text style={adding && styles.buttonTextDisabled}>
+            {adding ? 'Adding…' : 'Add'}
+          </Text>
+        </Pressable>
+      </View>
+      {updateErrorMessage && (
+        <Text style={[styles.message, styles.error]}>{updateErrorMessage}</Text>
+      )}
       <FlatList
         data={restaurants}
         keyExtractor={item => item.id}
@@ -65,6 +75,7 @@ export default function RestaurantList({
           <View style={styles.restaurantRow}>
             <Text style={styles.restaurantName}>{item.name}</Text>
             <Pressable
+              style={styles.button}
               onPress={() =>
                 api
                   .delete(`/restaurants/${item.id}`)
@@ -81,11 +92,40 @@ export default function RestaurantList({
           </View>
         )}
       />
-    </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    backgroundColor: '#eee',
+  },
+  buttonTextDisabled: {
+    color: '#999',
+  },
+  container: {
+    flex: 1,
+  },
+  addRow: {
+    flexDirection: 'row',
+    padding: 8,
+  },
+  newRestaurantNameField: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 4,
+  },
+  addButton: {
+    marginLeft: 8,
+  },
   message: {
     fontSize: 18,
     padding: 8,
@@ -96,11 +136,14 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   restaurantRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderBottomWidth: 1,
     borderColor: '#ccc',
     padding: 8,
   },
   restaurantName: {
+    flex: 1,
     fontSize: 18,
   },
 });
