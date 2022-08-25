f426186f0fb571f01e5fb2d4737a491fd93c1eaa
Josh Justice <me@codingitwrong.com>
2022-08-25 05:52:28 -0600

    Display restaurant names

--- src/RestaurantList.spec.js
+++ src/RestaurantList.spec.js
@@ -6,4 +6,13 @@ describe('RestaurantList', () => {
     {id: 1, name: 'Pizza Place'},
     {id: 2, name: 'Salad Place'},
   ];
+
+  describe('when restaurants are available', () => {
+    it('displays the restaurant names', () => {
+      render(<RestaurantList restaurants={restaurants} />);
+
+      expect(screen.queryByText(restaurants[0].name)).toBeTruthy();
+      expect(screen.queryByText(restaurants[1].name)).toBeTruthy();
+    });
+  });
 });
