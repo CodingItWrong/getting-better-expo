3bcce3b269d75905b4825b5808355661c046a0ba
Josh Justice <me@codingitwrong.com>
2022-08-25 19:18:07 -0600

    Test delete failure

--- src/RestaurantRow.spec.js
+++ src/RestaurantRow.spec.js
@@ -1,4 +1,9 @@
-import {fireEvent, render, screen} from '@testing-library/react-native';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react-native';
 import RestaurantRow from './RestaurantRow';
 
 describe('RestaurantRow', () => {
@@ -20,5 +25,27 @@ describe('RestaurantRow', () => {
     expect(screen.queryByText('Deleting…')).toBeTruthy();
 
     expect(onDelete).toHaveBeenCalledWith();
+  });
+
+  it('re-enables the button if deleting fails', async () => {
+    const onDelete = jest.fn().mockName('onDelete').mockRejectedValue();
+
+    render(<RestaurantRow restaurant={restaurant} onDelete={onDelete} />);
+
+    fireEvent.press(screen.getByText('Delete'));
+
+    expect(screen.queryByText('Deleting…')).toBeTruthy();
+    expect(screen.getByTestId('delete-button')).toHaveProp(
+      'accessibilityState',
+      {disabled: true},
+    );
+
+    expect(onDelete).toHaveBeenCalledWith();
+
+    await waitForElementToBeRemoved(() => screen.getByText('Deleting…'));
+    expect(screen.getByTestId('delete-button')).toHaveProp(
+      'accessibilityState',
+      {disabled: false},
+    );
   });
 });
